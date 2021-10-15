e6ce19a243ad5908306945c1a9e44c7bd05c0e4f
nebulazc <67900555+nebulazc@users.noreply.github.com>
2021-10-15 15:28:34 +0200

    3

--- js/tic-tac-toe/index.js
+++ js/tic-tac-toe/index.js
@@ -3,6 +3,7 @@ let symbols = [["", "", ""], ["", "", ""], ["", "", ""]];
 
 const board = document.querySelector(".board");
 const tiles = Array.from(document.querySelectorAll(".tile"));
+const resetButton = document.querySelector(".reset");
 
 board.addEventListener("click", ({ target }) => {
   const classes = Array.from(target.classList);
@@ -20,14 +21,38 @@ board.addEventListener("click", ({ target }) => {
 });
 
 function displayTurn(turn) {
-  // 1. zmień text elementu h1 z klasą "turn" zależnie od tego, czyja jest aktualnie tura
+  document.querySelector(".turn").innerText = `${turn} turn`;
 }
 
 function checkWin() {
   // 2. sprawdź czy któryś z graczy wygrał pojedynek - jeśli tak wyświetla komunikat (możesz użyć np. funkcji "alert(...)")
+  for (let i = 0; i < 3; i++) {
+    console.log(symbols);
+    // column check
+    const col = symbols[i];
+    if (col[0] != "" && col[0] === col[1] && col[1] === col[2]) {
+      alert(`${col[0]} won`);
+    }
+    // row check
+    if (symbols[0][i] != "" && symbols[0][i] === symbols[1][i] && symbols[1][i] === symbols[2][i]) {
+      alert(`${symbols[0][i]} won`);
+    }
+  }
+  // diagonal check
+  if (symbols[0][0] != "" && symbols[0][0] == symbols[1][1] && symbols[1][1] == symbols[2][2]) {
+    alert(`${symbols[0][0]} won`);
+  } else if (symbols[0][2] != "" && symbols[0][2] == symbols[1][1] && symbols[1][1] == symbols[2][0]) {
+    alert(`${symbols[0][2]} won`);
+  }
+
 }
 
 // 3. dodaj listener pod przycisk z napisaem "reset" tak, aby po jego kliknięciu wywołać funkcję reset
 function reset() {
-  // 4. zresetuj stan gry
+  symbols = [["", "", ""], ["", "", ""], ["", "", ""]];
+  tiles.forEach(element => {
+    element.classList = "tile";
+  });
 }
+
+resetButton.addEventListener("click", reset);
