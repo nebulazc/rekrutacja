644d1149960a504cfab9e67704e4a5f8587e898e
nebulazc <67900555+nebulazc@users.noreply.github.com>
2021-10-15 15:01:30 +0200

    2

--- js/riddles/index.js
+++ js/riddles/index.js
@@ -1,7 +1,10 @@
 // 1. odwróć liczbę
 // np dla 12345, funkcja powinna zwrócić 54321
-function reverseNumber() {
-  // uzupełnij tutaj
+function reverseNumber(x) {
+  let strx = x.toString();
+  let arrstrx = strx.split("");
+  arrstrx = arrstrx.reverse();
+  return parseInt(arrstrx.join(""));
 }
 
 console.log("1.", reverseNumber(12345));
@@ -10,7 +13,11 @@ console.log("1.", reverseNumber(12345));
 // dla tablicy tab powinniśmy otrzymać 2 + 4 + 6 + 8 = 20
 const tab = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function addEven(array) {
-  // uzupełnij tutaj
+  let sum = 0;
+  array.forEach(elem => {
+    if (elem % 2 === 0) sum += elem; 
+  });
+  return sum;
 }
 
 console.log("2.", addEven(tab));
